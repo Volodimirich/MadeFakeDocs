fcdecd1910c1cffdc0cc4b153a3792b0799cd373
Volodimirich <shaposh-vova@rambler.ru>
2023-05-12 20:56:49 +0300

    t5 fix

--- src/modules/model.py
+++ src/modules/model.py
@@ -4,15 +4,15 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer, T5ForConditionalGenerat
 def get_tokenizer(tokenizer_name):
     if tokenizer_name == 'gpt2':
         return GPT2Tokenizer.from_pretrained('gpt2')
-    elif tokenizer_name == 't5_v1_1-large':
-        return T5Tokenizer.from_pretrained("google/t5-v1_1-large")
+    elif tokenizer_name in ('t5_v1_1-large', 't5_v1_1-small'):
+        return T5Tokenizer.from_pretrained("google/{}")
 
 
 def get_model(model_name, device, local_path='', is_local=False):
     if model_name == 'gpt2':
         model = local_path if is_local else model_name
         return GPT2LMHeadModel.from_pretrained(model).to(device)
-    elif model_name == 't5_v1_1-small':
-        return T5ForConditionalGeneration.from_pretrained("google/t5-v1_1-small").to(device)  
+    elif model_name == ('t5_v1_1-large', 't5_v1_1-small'):
+        return T5ForConditionalGeneration.from_pretrained(f"google/{model_name}").to(device)  
     
     
